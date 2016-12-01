df1f41fcbe7d3a915018e6467a9cb3b8fff89e34
mblancosoto <mblancosoto@victoria24.danielcastelao.org>
2016-12-01 09:12:23 +0100

    refactorizado 1

--- refactorizacion/src/refactorizacion/Refactor.java
+++ refactorizacion/src/refactorizacion/Refactor.java
@@ -14,7 +14,8 @@ import javax.swing.JOptionPane;
 public class Refactor {
     
     private int numDigitos,nDigitos;
-    private static boolean p = false;
+    private boolean esprimo = false;
+    
     
     public Refactor(){
         
@@ -31,22 +32,58 @@ public class Refactor {
     
     public int pedirNumdigito(){
        
-       int  numDigitos = Integer.parseInt(JOptionPane.showInputDialog("Introduce los dígitos"));     
-
-                
+       int  numDigitos = Integer.parseInt(JOptionPane.showInputDialog("Introduce los dígitos"));    
+       while(numDigitos<0|numDigitos>5){
+           numDigitos= Integer.parseInt(JOptionPane.showInputDialog("Introducie cifras entre 0 y 5"));
+       }
            return numDigitos;
       
     }
     
    
-       public void metodoPrimos(){
-           
-           
-           
-       } 
+   public int calculaDigitos(int numero){
+       
+               int divisionEntera = numero;
+ 
+            int contador=0;
+ 
+            while (divisionEntera != 0)
+        {
+            divisionEntera = divisionEntera / 10;
+            contador++;
+        }
+       return contador;
+   } 
      
-     
-    
-    
+    public void limite (int i){
+                    boolean numPrimo =false;
+                    int contador1 = 0;  
+                    int i1 = 1; 
+                    int limite = (i - 1) / 2; 
+                    if  (limite % 2 == 0) limite--;  
+             
+                    while(i1 <= limite)
+                    {
+                        if (i % i1 == 0) contador1++;
+                            i1 += 2;
+                            if (contador1 == 2) i1 = limite + 1;
+                    }
+ 
+        if (contador1 == 1) numPrimo = true;
+        
+        
+              if (numPrimo == true){
+                    System.out.println(i);  
+        
+            } 
+        
+       
+            }
+       
+    }
 
-}
+      
+
+         
+         
+    
--- refactorizacion/src/refactorizacion/Refactorizacion.java
+++ refactorizacion/src/refactorizacion/Refactorizacion.java
@@ -17,55 +17,25 @@ public class Refactorizacion {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Refactor obj = new Refactor();
+        Refactor obj = new Refactor();    
+        boolean numPrimo = false;
+                
         int numDigitos= obj.pedirNumdigito();
         int ndigitos=0;
-        if(numDigitos<=0)
-        System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
         for(int i = 1; i <= 99999; i++ )
-        {
-            int divisionEntera = i;
- 
-            int contador=0;
- 
-            while (divisionEntera != 0)
-        {
-            divisionEntera = divisionEntera / 10;
-            contador++;
-        }
-        ndigitos=contador;
-            
-            
-            if(ndigitos==numDigitos){
-                boolean numPrimo = false;
-                   if (i < 4) numPrimo = true;
-            else
-            {
-            if (i % 2 == 0) numPrimo = false; 
-                else
-                {
-                    int contador1 = 0;  
-                    int i1 = 1; 
-                    int limite = (i - 1) / 2; 
-                    if  (limite % 2 == 0) limite--;  
-             
-                    while(i1 <= limite)
-                    {
-                        if (i % i1 == 0) contador1++;
-                            i1 += 2;
-                            if (contador1 == 2) i1 = limite + 1;
-                    }
- 
-        if (contador1 == 1) numPrimo = true;
-            } 
+        {           
+        ndigitos=obj.calculaDigitos(i);
+                     
+         if(ndigitos==numDigitos){
+             obj.limite(i);
         } 
      
                 if (numPrimo == true) 
                     System.out.println(i);    
             }
         }
-        }
- 
-    }
+        
+}
+    
     
 
